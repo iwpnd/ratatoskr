cd0679b8bc34eccba4d09c97646b9e266cc291ec
Benjamin Ramser <legionaerr@googlemail.com>
2024-06-02 13:52:03 +0200

    chore: test builder

--- bin/builder/main.go
+++ bin/builder/main.go
@@ -5,25 +5,46 @@ import (
 	"log/slog"
 	"os"
 
-	"github.com/iwpnd/valhalla-builder/services"
+	"github.com/iwpnd/valhalla-builder/services/download"
+	"github.com/iwpnd/valhalla-builder/services/tiles"
 	"github.com/iwpnd/valhalla-builder/states"
 )
 
 func main() {
+	ctx := context.Background()
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	blobStore := services.NewBlobStore("blobstore", logger)
-	fileStore := services.NewFileStore("filestore", logger)
+	name := "europe/andorra"
 
-	name := "baguette"
-	args := states.Args{Name: name, Logger: logger, BlobStore: blobStore, FileStore: fileStore}
+	path := "./bin/builder/data"
 
-	// deadline := 1000 * time.Millisecond
-	ctx := context.Background()
-	// ctx, cancelCtx := context.WithTimeout(ctx, deadline)
-	// defer cancelCtx()
+	builderOpts := &tiles.TileBuilderOptions{
+		Path:         path,
+		Concurrency:  6,
+		MaxCacheSize: 700 * 1048576, // 700MiB
+		Debug:        false,
+		Dataset:      "andorra-latest.osm.pbf",
+	}
+	builder, err := tiles.NewTileBuilder(builderOpts, logger)
+	if err != nil {
+		logger.Error("cannot instantiate executor: ", "err", err)
+		panic(err)
+	}
 
-	err := states.Execute(ctx, args)
+	downloaderOpts := download.GeofabrikDownloaderOptions{
+		Dataset:    name,
+		OutputPath: path,
+	}
+	downloader := download.NewGeofabrikDownloader(downloaderOpts)
+
+	args := states.Args{
+		Name:       name,
+		Logger:     logger,
+		Builder:    builder,
+		Downloader: downloader,
+	}
+
+	err = states.Execute(ctx, args)
 	if err != nil {
 		slog.Error("something went wrong", "error", err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/iwpnd/valhalla-builder
 
 go 1.22.3
+
+require (
+	github.com/iwpnd/go-geofabrik v0.2.0 // indirect
+	github.com/iwpnd/rip v0.5.0 // indirect
+)
